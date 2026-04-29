6005ed07851333353b42fe93de58bba2d28ad9fb
AldoCristianSimanungkalit <cristianaldo444@gmail.com>
2026-04-29 14:21:00 +0700

    perubahan8

--- Latihan/resources/views/pages/home.blade.php
+++ Latihan/resources/views/pages/home.blade.php
@@ -701,7 +701,7 @@
         <div class="hero-content">
             <div>
                 <div class="hero-subtitle"> Global Geopark</div>
-                <h1 class="hero-title">   TELE · EFRATA ·  </h1>
+                <h1 class="hero-title">   TELE · EFRATA · SIHOTANG </h1>
                 <div class="hero-divider"></div>
                 <a href="#destinasi" class="hero-btn">Jelajahi Sekarang</a>
             </div>
@@ -806,7 +806,7 @@
                 <!-- Sihotang -->
                 <div class="destinasi-item" data-aos="fade-up">
                     <div class="destinasi-image">
-                        <img src="/image/tele/tele.jpeg" alt="Sihotang">
+                        <img src="/image/about-toba.jpg" alt="Sihotang">
                     </div>
                     <div class="destinasi-content">
                         <div class="destinasi-number">03 — GEOSITE</div>
