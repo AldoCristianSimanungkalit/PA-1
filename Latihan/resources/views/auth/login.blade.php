522f7de755fd6e97e02eabd02e7dbe08c1780b5e
AldoCristianSimanungkalit <cristianaldo444@gmail.com>
2026-04-29 20:15:27 +0700

    perubahan13

--- Latihan/resources/views/auth/login.blade.php
+++ Latihan/resources/views/auth/login.blade.php
@@ -22,11 +22,11 @@
                             @csrf
                             <div class="mb-3">
                                 <label>Email</label>
-                                <input type="email" name="email" class="form-control" required>
+                                <input type="email" name="kelompok9" class="form-control" required>
                             </div>
                             <div class="mb-3">
                                 <label>Password</label>
-                                <input type="password" name="password" class="form-control" required>
+                                <input type="password" name="123" class="form-control" required>
                             </div>
                             <button type="submit" class="btn btn-dark w-100">Login</button>
                         </form>
